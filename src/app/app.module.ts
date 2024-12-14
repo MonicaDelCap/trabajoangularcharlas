f2709c61d42f166e66288b05902924652bdefdce
ItxChewy <aarongarcia1906@gmail.com>
2024-12-14 13:27:42 +0100

    perfil_charlas v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { ServiceRound } from './services/service.round';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ServiceCharla } from './services/charla.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     MatCardModule, 
     MatButtonModule
   ],
-  providers: [ServiceUser, ServiceRound],
+  providers: [ServiceUser, ServiceRound,ServiceCharla],
   bootstrap: [AppComponent],
   
 })
