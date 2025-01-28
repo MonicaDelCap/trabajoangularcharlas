d40c9ff674556bfd9425e7250b09666e457d0ce0
MonicaDelCap <monicadelgado437@gmail.com>
2025-01-28 16:43:59 +0100

    rondas con mas info y funcionalidades

--- src/app/components/rondasstudent/rondasstudent.component.ts
+++ src/app/components/rondasstudent/rondasstudent.component.ts
@@ -7,66 +7,86 @@ import { TalksByCourse } from '../../models/talks';
 import { ServiceTalks } from '../../services/service.talks';
 import { environment } from '../../../environments/environment';
 import { Charla } from '../../models/charla';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-rondasstudent',
   templateUrl: './rondasstudent.component.html',
   styleUrl: './rondasstudent.component.css'
 })
-export class RondasstudentComponent implements OnInit{
+export class RondasstudentComponent implements OnInit {
 
-  inicio!: Date;
-  final!:  Date;
+  diferenciaDiasVotacion !: number;
+  diferenciaDiasAniadirCharla !: number;
+  fechaLimiteAniadirCharla !: string;
   idRonda!: number;
   round !: Round;
   talks: Charla[] = []; // Todas las charlas
-  
+
   didUAddTalk!: boolean;
   isButtonDisabled = false;
 
   constructor(
     private _serviceRound: ServiceRound,
     private _serviceTalks: ServiceTalks,
-    private _router:Router,
-    private _active:ActivatedRoute
-  ){
+    private _router: Router,
+    private _active: ActivatedRoute,
+    private datePipe: DatePipe
+  ) {
     this.didUAddTalk = false;
   }
 
 
   ngOnInit(): void {
-    if(!localStorage.getItem('authToken')){
+    if (!localStorage.getItem('authToken')) {
       this._router.navigate(["/"])
     }
-    
+
     this._active.params.subscribe((params: Params) => {
       this.idRonda = params['id']
       this._serviceTalks.getTalks(this.idRonda).then(r => {
         this.talks = r;
         this.didUAddTalk = this.searchTalkInRound();
-        this._serviceRound.getRoundById(this.idRonda).then( r => this.round = r)
+        this._serviceRound.getRoundById(this.idRonda).then(r => {
+          this.round = r;
+          this.diferenciaDiasVotacion = this.calculateDaysToExpire(new Date(this.round.fechaLimiteVotacion), new Date())
+          this.diferenciaDiasAniadirCharla = this.calculateDaysToExpire(new Date(this.round.fechaCierre), new Date())
+          if(this.diferenciaDiasAniadirCharla <= 0 || this.didUAddTalk || this.diferenciaDiasVotacion <= 0){
+            this.didUAddTalk = true;
+          }else{
+            this.didUAddTalk = false;
+          }
+          
+          this.fechaLimiteAniadirCharla ="Fecha limite para añadir charla: " + this.convertDate(this.round.fechaCierre)
+          
+        })
       })
     })
 
   }
 
-  calculateDaysToExpire(dateinit:Date, dateend: Date):number{
-    console.log("hola")
-    let diferenciaMilisegundos = this.final.getTime()- this.inicio.getTime();
+  calculateDaysToExpire(dateinit: Date, dateend: Date): number {
+    let diferenciaMilisegundos =  dateinit.getTime() - dateend.getTime();
 
     let diferenciaDias = diferenciaMilisegundos / (1000 * 60 * 60 * 24);
 
+    console.log(dateinit)
+    console.log(dateend)
     console.log(Math.round(diferenciaDias))
     return Math.round(diferenciaDias);
   }
 
-  searchTalkInRound():boolean{
-    for(let talk of this.talks){
-      if(talk.idUsuario == environment.idUsuario){
+  searchTalkInRound(): boolean {
+    for (let talk of this.talks) {
+      if (talk.idUsuario == environment.idUsuario) {
         console.log(talk.idUsuario);
         return true;
       }
     }
     return false;
+  }
+
+  convertDate(date: string): string {
+    return this.datePipe.transform(date, 'dd/MM/yyyy')!;
   }
 
 
