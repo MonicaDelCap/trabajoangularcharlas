d921275c4b33c55188016aad681550483877341c
MonicaDelCap <monicadelgado437@gmail.com>
2025-01-31 08:31:06 +0100

    fallos de ultima hora

--- src/app/components/courseswithstudentdisable/courseswithstudentdisable.component.ts
+++ src/app/components/courseswithstudentdisable/courseswithstudentdisable.component.ts
@@ -14,63 +14,66 @@ import { ServiceAdmin } from '../../services/service.admin';
 })
 export class CourseswithstudentdisableComponent implements OnInit {
   public students!: Array<Student>
-  public idCourse!:number;
-  public courses!:Array<StudentsCoursesTeacher>
+  public idCourse!: number;
+  public courses!: Array<StudentsCoursesTeacher>
   public isCourse!: StudentsCoursesTeacher
   public disable: string = "disable";
-  public role: number = environment.idUsuario;
-    constructor(
-      private _serviceTeacher: ServiceTeacherM,
-      private _serviceAdmin: ServiceAdmin,
-      private _active:ActivatedRoute,
-      private _serviceRound:ServiceRound,
-      private _router:Router
-    ) { 
-      this.students = new Array<Student>;
-    }
-   ngOnInit(): void {
+  public role!: number;
+  constructor(
+    private _serviceTeacher: ServiceTeacherM,
+    private _serviceAdmin: ServiceAdmin,
+    private _active: ActivatedRoute,
+    private _serviceRound: ServiceRound,
+    private _router: Router
+  ) {
+    this.students = new Array<Student>;
+  }
+  ngOnInit(): void {
     if (!localStorage.getItem('authToken')) {
       this._router.navigate(["/"])
     }
-      this._active.params.subscribe((params: Params) => {
-        this.idCourse = params["id"];
-        if(this.role == 2){
-          this._serviceTeacher.getStudentsCourseDisable().then(r => {
-            this.courses = r;
-            this.selectCourse();
-          })
-        }else{
-          this._serviceAdmin.getUsuariosCurso(this.idCourse).subscribe(r => this.students = r)
-        }
-        
-      })
-    
-    }
-  
-   
-    selectCourseWithTeacher():void{
-      let estudiantes = this.students;
-      this.students = new Array<Student>
-      for(let stu of estudiantes){
-        if(stu.idRole == 1 ){
-          
-        }else{
-          this.students.push(stu)
-        }
+    const role = localStorage.getItem('role');
+    this.role = role ? parseInt(role, 10) : 0;
+    console.log(role)
+    this._active.params.subscribe((params: Params) => {
+      this.idCourse = params["id"];
+      if (this.role == 2) {
+        this._serviceTeacher.getStudentsCourseDisable().then(r => {
+          this.courses = r;
+          this.selectCourse();
+        })
+      } else {
+        this._serviceAdmin.getUsuariosCurso(this.idCourse).subscribe(r => this.students = r)
+      }
+
+    })
+
+  }
+
+
+  selectCourseWithTeacher(): void {
+    let estudiantes = this.students;
+    this.students = new Array<Student>
+    for (let stu of estudiantes) {
+      if (stu.idRole == 1) {
+
+      } else {
+        this.students.push(stu)
       }
     }
-  
-    selectCourse():boolean{
-      for(let course of this.courses){
-  
-        if(course.curso != null && course.curso.idCurso == this.idCourse){
-          this.isCourse = course
-          for(let student of course.alumnos){
-            this.students.push(student.alumno)
-          }
-          return true;
+  }
+
+  selectCourse(): boolean {
+    for (let course of this.courses) {
+
+      if (course.curso != null && course.curso.idCurso == this.idCourse) {
+        this.isCourse = course
+        for (let student of course.alumnos) {
+          this.students.push(student.alumno)
         }
+        return true;
       }
-      return false;
     }
+    return false;
+  }
 }
